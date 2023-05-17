18c695170b448e2cac3ad2dab7006a317cd6dd57
Maxence Badin Léger <maxencelger@gmail.com>
2023-05-17 11:25:35 +0200

    initialisation

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const header = () => {
+  return (
+    <header>
+      <a href="https://github.com/Mahkalix/lofi" target="_blank">
+        <img src="src/assets/logolofi.jpg" className="github-link" />
+      </a>
+    </header>
+  );
+};
+
+export default header;
