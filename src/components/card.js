a33039ae309dc9167530edf91ab711dec3548378
Maxence Badin Léger <maxencelger@gmail.com>
2023-06-26 16:51:10 +0200

    finally it's working

--- src/components/card.js
+++ src/components/card.js
@@ -5,17 +5,19 @@ import PauseIcon from "../assets/svg/pause.svg";
 
 const Card = ({ musicData, play, isPlaying }) => {
   return (
-    <div onClick={play} className={`card ${isPlaying ? "playing" : ""}`} alt="">
-      <img className="gif" src={musicData.gif} alt="gif" />
-      {!isPlaying ? (
-        <img className="Pause" src={PauseIcon} alt="Pause" />
-      ) : (
-        <img className="Play" src={PlayIcon} alt="Play" />
-      )}
-      <h1 className="title" title={musicData.title}>
-        {musicData.title}
-      </h1>
-    </div>
+    <>
+      <button onClick={play} className="card" alt="gif">
+        <img className="gif" src={musicData.gif} alt="gif" />
+        {!isPlaying ? (
+          <img className="Pause" src={PauseIcon} alt="Pause" />
+        ) : (
+          <img className="Play" src={PlayIcon} alt="Play" />
+        )}
+        <h1 className="title" title={musicData.title}>
+          {musicData.title}
+        </h1>
+      </button>
+    </>
   );
 };
 
