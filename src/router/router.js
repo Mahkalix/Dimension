f6c9ac07f78837a603ba18a38d9fbf5288c83e5a
Maxence Badin Léger <maxencelger@gmail.com>
2023-06-26 18:28:46 +0200

    build update

--- src/router/router.js
+++ src/router/router.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 
-const router = () => {
+const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
@@ -10,4 +10,4 @@ const router = () => {
   );
 };
 
-export default router;
+export default Router;
