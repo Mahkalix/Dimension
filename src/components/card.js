3569d0851d9c23749456c722a3dd5543e4fdf69c
Maxence Badin Léger <maxencelger@gmail.com>
2023-06-26 12:18:00 +0200

    style btn done

--- src/components/card.js
+++ src/components/card.js
@@ -1,11 +1,18 @@
 import React from "react";
+import "../style/card.css";
+import PlayIcon from "../assets/svg/play.svg";
+import PauseIcon from "../assets/svg/pause.svg";
 
 const Card = ({ musicData, play, isPlaying }) => {
   return (
     <>
       <button onClick={play} className="card" alt="gif">
-        {isPlaying ? "Pause" : "Play"}
         <img className="gif" src={musicData.gif} alt="gif" />
+        {!isPlaying ? (
+          <img className="Pause" src={PauseIcon} alt="Pause" />
+        ) : (
+          <img className="Play" src={PlayIcon} alt="Play" />
+        )}
         <h1 className="title" title={musicData.title}>
           {musicData.title}
         </h1>
