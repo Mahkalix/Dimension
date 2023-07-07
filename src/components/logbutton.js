1d8817f3cd26e6cabe3662b73e233f89df050690
Maxence Badin Léger <maxencelger@gmail.com>
2023-07-07 17:19:29 +0200

    page loading error done

--- src/components/logbutton.js
+++ src/components/logbutton.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import "../style/logbutton.css";
-import Acceuil from "../pages/Acceuil";
 
 import arrow from "../assets/svg/arrow-right.svg";
 
--- src/router/router.js
+++ src/router/router.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
+import Accueil from "../pages/Accueil";
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/Accueil" element={<Accueil />} />
     </Routes>
   );
 };
